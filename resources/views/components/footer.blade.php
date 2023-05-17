1b27baca03fe9d757e25705349372ec196ff652c
tenyuoooogi <tanjoubi0103@gmail.com>
2023-05-17 19:29:47 +0900

    10章フッター

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,5 @@
+<nav class="navbar navbar-expend-md navbar-light bg-white shadow-sm fixed-bottom mt-5">
+
+<a class="navbar-brand mx-auto" href="{{url('/')}}">
+
+<img src="{{asset('img/logo.jpg')}}" class="img-fluid">

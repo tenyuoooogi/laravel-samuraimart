7c82391e960528b73f07df12dc2c5f4febeed484
tenyuoooogi <tanjoubi0103@gmail.com>
2023-05-21 16:13:27 +0900

    15章　ソート

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,9 +1,10 @@
 <div class="container">
      @foreach ($major_category_names as $major_category_name)
-        <h2>{{ $major_category_name }}</h2>
+         <h2>{{ $major_category_name }}</h2>
          @foreach ($categories as $category)
-        <label class="samuraimart-sidebar-category-label"><a href="{{ route('products.index', ['category' => $category->id]) }}">{{ $category->name }}</a></label>
-       @endif
-      @endforeach
+             @if ($category->major_category_name === $major_category_name)
+                 <label class="samuraimart-sidebar-category-label"><a href="#">{{ $category->name }}</a></label>
+             @endif
+         @endforeach
      @endforeach
  </div>

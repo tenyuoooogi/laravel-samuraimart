4d023202eb99591bc106c87fe82c3bd2656e1a38
tenyuoooogi <tanjoubi0103@gmail.com>
2023-05-28 22:42:38 +0900

    6章

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,11 @@
     <div class="row justify-content-center">
         <div class="col-md-5">
             <h3 class="mt-3 mb-3">ログイン</h3>
-
+            @if (session('warning'))
+             <div class="alert alert-danger">
+                 {{ session('warning') }}
+             </div>
+             @endif
             <hr>
             <form method="POST" action="{{ route('login') }}">
                 @csrf

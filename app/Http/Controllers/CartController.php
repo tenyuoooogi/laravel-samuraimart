616ba2357b2c1fb123255d9e4b94c5c9e65da533
tenyuoooogi <tanjoubi0103@gmail.com>
2023-05-30 22:54:18 +0900

    8章　エラー　nullのプロパティ「id」を読み取ろうとしました（ cartcontroller)

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -20,11 +20,20 @@ class CartController extends Controller
         $cart = Cart::instance(Auth::user()->id)->content();
 
         $total = 0;
+        $has_carriage_cost = false;
+        $carriage_cost = 0;
 
         foreach ($cart as $c) {
             $total += $c->qty * $c->price;
+            if ($c->options->carriage) {
+                $has_carriage_cost = true;
+            }
         }
-        return view('carts.index',compact('cart','total'));
+        if($has_carriage_cost) {
+            $total += env('CARRIAGE');
+            $carriage_cost = env('CARRIAGE');
+        }
+        return view('carts.index', compact('cart', 'total', 'carriage_cost'));
     }
 
    
@@ -44,9 +53,10 @@ class CartController extends Controller
                 'price' => $request->price, 
                 'weight' => $request->weight, 
                 'options' => [
-                    
+
                     'image' => $request->image,
-                ]
+                    'carriage' => $request->carriage,
+                    ]
             ] 
         );
 

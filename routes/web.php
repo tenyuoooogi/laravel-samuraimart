d6afe3fae319fd098ce5270d878ce0282eb0b122
tenyuoooogi <tanjoubi0103@gmail.com>
2023-06-05 23:10:05 +0900

    12章　無効なルート アクション: [App\Admin\Controllers\register_card]。 エラー

--- routes/web.php
+++ routes/web.php
@@ -36,6 +36,9 @@ Route::controller(UserController::class)->group(function () {
     Route::get('users/mypage/favorite', 'favorite')->name('mypage.favorite');
     Route::delete('users/mypage/delete', 'destroy')->name('mypage.destroy');
     Route::get('users/mypage/cart_history', 'cart_history_index')->name('mypage.cart_history');
+    Route::get('users/mypage/cart_history/{num}', 'cart_history_show')->name('mypage.cart_history_show');
+    Route::get('users/mypage/register_card', 'register_card')->name('mypage.register_card');
+    Route::post('users/mypage/token', 'token')->name('mypage.token');
 
 });
 
